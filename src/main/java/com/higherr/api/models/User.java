66fd2184c34f5483e4abfe1f88c6f180b3f9592c
Hat Dao <enjoy3013@gmail.com>
2017-04-09 13:27:45 +0800

    use postgresql

--- src/main/java/com/higherr/api/models/User.java
+++ src/main/java/com/higherr/api/models/User.java
@@ -3,11 +3,13 @@ package com.higherr.api.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "HigherrUser")
 public class User {
 
     private @Id @GeneratedValue Long id;
